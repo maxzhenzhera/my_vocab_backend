6480a17742fb205521587c2bfc246c7f458ea220
Max Zhenzhera <megashurik@urk.net>
2021-09-06 14:09:19 +0300

    add db_sessionmaker to __all__

--- app/db/session.py
+++ app/db/session.py
@@ -7,5 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from ..core.config import sqlalchemy_connection_string
 
 
+__all__ = ['db_sessionmaker']
+
 engine = create_async_engine(sqlalchemy_connection_string)
 db_sessionmaker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)

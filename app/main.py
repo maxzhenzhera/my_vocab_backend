ac4561b182adc08699c241d4fc4191e9fd056795
Max Zhenzhera <megashurik@urk.net>
2021-09-09 20:33:07 +0300

    include main api router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 
-from app.core.config import uvicorn_config
+from app.core.config import (
+    server_config,
+    uvicorn_config
+)
+from app.api.routes import router
 
 
-app = FastAPI()
+def get_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(router, prefix=server_config.API_PREFIX)
+    return application
+
+
+app = get_application()
 
 
 if __name__ == '__main__':

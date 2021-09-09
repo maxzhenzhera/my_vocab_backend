78326ddde30d8605723822354544ed11763f2206
Max Zhenzhera <megashurik@urk.net>
2021-09-09 23:06:55 +0300

    add user password service

---
+++ app/services/security.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+import bcrypt
+from passlib.context import CryptContext
+
+from ..db.models import User
+
+
+__all__ = ['UserPasswordService']
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+@dataclass
+class UserPasswordService:
+    user: User
+
+    def change_password(self, password: str) -> User:
+        self.user.password_salt = self._generate_salt()
+        self.user.hashed_password = self._get_password_hash(self._combine_password_with_salt(password=password))
+        return self.user
+
+    def verify_password(self, password: str) -> bool:
+        return self._verify_password(self._combine_password_with_salt(password=password), self.user.hashed_password)
+
+    def _combine_password_with_salt(self, password: str) -> str:
+        return self.user.password_salt + password
+
+    @staticmethod
+    def _generate_salt() -> str:
+        return bcrypt.gensalt().decode()
+
+    @staticmethod
+    def _get_password_hash(password: str) -> str:
+        return pwd_context.hash(password)
+
+    @staticmethod
+    def _verify_password(plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)

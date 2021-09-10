b49d716638813f1027dd1f28346f668887129410
Max Zhenzhera <megashurik@urk.net>
2021-09-10 13:15:51 +0300

    add user schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    UUID4
+)
+
+from .base import (
+    IDModelMixin,
+    ModelWithOrmMode
+)
+
+
+__all__ = [
+    'UserInCreate',
+    'UserInLogin',
+    'UserInUpdate',
+    'UserInResponse'
+]
+
+
+class UserInCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserInLogin(UserInCreate):
+    pass
+
+
+class UserInUpdate(BaseModel):
+    email: Optional[EmailStr]
+    password: Optional[str]
+
+
+class UserInResponse(IDModelMixin, ModelWithOrmMode):
+    email: EmailStr
+    activation_link: UUID4
+    is_active: bool
+    is_email_confirmed: bool
+    is_superuser: bool
+    email_confirmed_at: Optional[datetime]

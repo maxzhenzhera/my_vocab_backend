8cee52c085349df82f0a091691d910fe95df8116
Max Zhenzhera <megashurik@urk.net>
2021-09-10 13:44:00 +0300

    add db exceptions

---
+++ app/db/errors.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'DBError',
+    'EntityDoesNotExistError'
+]
+
+
+class DBError(Exception):
+    """ Common DB exception. """
+
+
+class EntityDoesNotExistError(DBError):
+    """ Raised if the searched entity does not exist in the database. """

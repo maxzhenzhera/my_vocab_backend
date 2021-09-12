b530a4d0edb230c91efc4621cfc6f19aa2a3c068
Max Zhenzhera <megashurik@urk.net>
2021-09-12 17:07:46 +0300

    add db dependency for getting repository

--- app/api/dependencies/db.py
+++ app/api/dependencies/db.py
@@ -1,8 +1,24 @@
+from typing import (
+    Callable,
+    Type
+)
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from ...db.repositories import BaseRepository
 from ...db.session import db_sessionmaker
+
+
+__all__ = ['get_repository']
 
 
 async def _get_session() -> AsyncSession:
     async with db_sessionmaker() as session, session.begin():
         yield session
+
+
+def get_repository(repository_type: Type[BaseRepository]) -> Callable[[AsyncSession], BaseRepository]:
+    def _get_repo(session: AsyncSession = Depends(_get_session)) -> BaseRepository:
+        return repository_type(session)
+    return _get_repo

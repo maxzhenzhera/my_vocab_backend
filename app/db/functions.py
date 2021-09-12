cdd1cc6027ba96ee6dfde9fb4c94d5ef36cf5a6e
Max Zhenzhera <megashurik@urk.net>
2021-09-12 18:19:20 +0300

    add utcnow function for server default

---
+++ app/db/functions.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql import expression
+from sqlalchemy.types import DateTime
+
+
+__all__ = ['utcnow']
+
+
+class utcnow(expression.FunctionElement):       # noqa
+    """ docs: https://docs.sqlalchemy.org/en/14/core/compiler.html#utc-timestamp-function """
+    type = DateTime()
+
+
+@compiles(utcnow, 'postgresql')
+def pg_utcnow(element, compiler, **kw):
+    return "TIMEZONE('utc', CURRENT_TIMESTAMP)"

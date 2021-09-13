92af9d1566bf7418e82f25b3af69c828c305e4c8
Max Zhenzhera <megashurik@urk.net>
2021-09-13 21:13:27 +0300

    add register view

--- app/api/routes/authentication.py
+++ app/api/routes/authentication.py
@@ -1,6 +1,20 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException
+)
+from starlette.status import HTTP_400_BAD_REQUEST
+
+from ...db.errors import EmailIsAlreadyTakenError
+from ...schemas.auth import AuthenticationResult
+from ...schemas.user import UserInCreate
+from ...services.auth import (
+    AuthenticationService,
+    CookieService
+)
+from ...services.mail import MailService
 
 
 __all__ = ['router']
@@ -11,6 +25,18 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.get('/test')
-async def test_endpoint():
-    return 1
+@router.post('/register', response_model=AuthenticationResult)
+async def register(
+        user_in_create: UserInCreate,
+        auth_service: AuthenticationService = Depends(),
+        cookie_service: CookieService = Depends(),
+        mail_service: MailService = Depends()
+):
+    try:
+        authentication_result = await auth_service.register(user_in_create)
+    except EmailIsAlreadyTakenError as error:
+        raise HTTPException(HTTP_400_BAD_REQUEST, error.detail)
+    else:
+        cookie_service.set_refresh_token(authentication_result.tokens.refresh_token)
+        mail_service.send_activation_mail(authentication_result.user)
+        return authentication_result

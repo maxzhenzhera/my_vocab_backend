3412cb50be021ca6251c8006e2d1fd1337d446d5
Max Zhenzhera <megashurik@urk.net>
2021-09-13 20:32:57 +0300

    add some repositories for short import

---
+++ app/db/repositories/__init__.py
@@ -0,0 +1,10 @@
+from .base import BaseRepository
+from .refresh_session import RefreshSessionsRepository
+from .user import UsersRepository
+
+
+__all__ = [
+    'BaseRepository',
+    'RefreshSessionsRepository',
+    'UsersRepository'
+]

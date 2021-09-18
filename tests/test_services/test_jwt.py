0726b8b5be371838ab66e733adb6695c183f32af
Max Zhenzhera <megashurik@urk.net>
2021-09-18 20:12:15 +0300

    add tests on tokens decoding/verifying; refactor

--- tests/test_services/test_jwt.py
+++ tests/test_services/test_jwt.py
@@ -2,6 +2,7 @@ from datetime import (
     datetime,
     timedelta
 )
+from time import sleep
 
 import pytest
 from jose import jwt
@@ -13,9 +14,15 @@ from app.schemas.jwt import (
     JWTUser
 )
 from app.services.jwt import UserJWTService
+from app.services.jwt.errors import (
+    InvalidTokenSignatureError,
+    ExpiredTokenSignatureError
+)
+from app.services.jwt.helpers import decode_token
 from app.services.jwt.types import (
     TokenPayloadMetaClaims,
     TokenDataForEncoding,
+    TokenDataForDecoding,
     Token,
     Tokens
 )
@@ -32,6 +39,44 @@ class TestUserJWTService:
     @pytest.fixture(name='service')
     def fixture_service(self, user: User) -> UserJWTService:
         return UserJWTService(user)
+
+    def test_tokens_verifying(self, service: UserJWTService):
+        tokens = service.generate_tokens()
+        assert isinstance(service.verify_access_token(tokens.access_token.token), JWTUser)
+        assert isinstance(service.verify_refresh_token(tokens.refresh_token.token), JWTUser)
+
+    def test_decode_token(self, service: UserJWTService):
+        common_test_payload = {
+            'sub': 'test',
+            'data': 'data'
+        }
+        test_token_expire_in_seconds = 1
+        test_payload_for_expire = {
+            'exp': service._compute_expire(timedelta(seconds=test_token_expire_in_seconds)),
+            **common_test_payload
+        }
+        test_payload_for_invalid_signature = {
+            'exp': service._compute_expire(timedelta(hours=1)),
+            **common_test_payload
+        }
+
+        test_token_for_expire = jwt.encode(test_payload_for_expire, self.TEST_TOKEN_SECRET, jwt_config.ALGORITHM)
+        with pytest.raises(ExpiredTokenSignatureError):
+            sleep(test_token_expire_in_seconds + 1)
+            decode_token(TokenDataForDecoding(test_token_for_expire, self.TEST_TOKEN_SECRET))
+
+        test_token_for_invalid_signature = jwt.encode(
+            test_payload_for_invalid_signature, self.TEST_TOKEN_SECRET, jwt_config.ALGORITHM
+        )
+        with pytest.raises(InvalidTokenSignatureError):
+            test_token_with_injection = test_token_for_invalid_signature + 'injection'
+            decode_token(TokenDataForDecoding(test_token_with_injection, self.TEST_TOKEN_SECRET))
+        with pytest.raises(InvalidTokenSignatureError):
+            test_token_invalid_secret = self.TEST_TOKEN_SECRET + 'invalid'
+            decode_token(TokenDataForDecoding(test_token_for_invalid_signature, test_token_invalid_secret))
+
+        normal_result = decode_token(TokenDataForDecoding(test_token_for_invalid_signature, self.TEST_TOKEN_SECRET))
+        assert isinstance(normal_result, dict)
 
     def test_jwt_user(self, service: UserJWTService):
         assert JWTUser.from_orm(service.user) == service.jwt_user
@@ -50,14 +95,12 @@ class TestUserJWTService:
 
         assert isinstance(service_token.expires_at, datetime)
         # GitHub Actions time difference on computing
-        # assert manual_token == service_token
         assert manual_token.token == service_token.token
 
     def test_prepare_payload(self, service: UserJWTService):
         test_payload = service._make_user_payload() | service._make_meta_payload(self.TEST_META_CLAIMS)
         service_payload = service._prepare_payload(self.TEST_META_CLAIMS)
         # GitHub Actions time difference on computing
-        # assert test_payload == service._prepare_payload(self.TEST_META_CLAIMS)
         del test_payload['exp']
         del service_payload['exp']
         assert test_payload == service_payload
@@ -70,6 +113,8 @@ class TestUserJWTService:
 
     def test_compute_expire(self):
         # GitHub Actions time difference on computing
-        # delta = timedelta(minutes=5)
-        # assert UserJWTService._compute_expire(delta) == datetime.utcnow() + delta
-        pass
+        computational_interference = timedelta(seconds=10)
+        test_delta = timedelta(minutes=5)
+        service_expire = UserJWTService._compute_expire(test_delta)
+        manual_expire = datetime.utcnow() + test_delta
+        assert (manual_expire - service_expire) < computational_interference

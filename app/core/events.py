68097c2b24a0cd4aa54a8dafb32057e555b76132
Max Zhenzhera <59729293+Max-Zhenzhera@users.noreply.github.com>
2021-09-19 19:20:20 +0300

    add mail sender initialization on startup

--- app/core/events.py
+++ app/core/events.py
@@ -6,6 +6,7 @@ from ..db.events import (
     init_db,
     close_db
 )
+from ..services.mail.events import init_mail_sender
 
 
 __all__ = [
@@ -17,6 +18,7 @@ __all__ = [
 def get_startup_handler(app: FastAPI) -> Callable:
     async def start_app() -> None:
         init_db(app)
+        init_mail_sender(app)
 
     return start_app
 

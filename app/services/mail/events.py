beb9371aa84c248b6fd424ae7612e42aeb2486dc
Max Zhenzhera <59729293+Max-Zhenzhera@users.noreply.github.com>
2021-09-19 19:19:17 +0300

    add mail service events

---
+++ app/services/mail/events.py
@@ -0,0 +1,17 @@
+import logging
+
+from fastapi import FastAPI
+from fastapi_mail import FastMail
+
+from ...core.config import mail_connection_config
+
+
+__all__ = ['init_mail_sender']
+
+
+logger = logging.getLogger(__name__)
+
+
+def init_mail_sender(app: FastAPI) -> None:
+    app.state.mail_sender = FastMail(mail_connection_config)
+    logger.info('Mail sender has been set.')

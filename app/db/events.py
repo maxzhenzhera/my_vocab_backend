ba2ba72747159c48565e5a1a7b9768ed94968284
Max Zhenzhera <59729293+Max-Zhenzhera@users.noreply.github.com>
2021-09-19 13:59:44 +0300

    add events functions for setting db in app on startup and shutdown; new architecture

---
+++ app/db/events.py
@@ -0,0 +1,29 @@
+import logging
+
+from fastapi import FastAPI
+
+from .postgres import (
+    create_engine,
+    create_sessionmaker
+)
+from ..core.config import sqlalchemy_connection_string
+
+
+__all__ = [
+    'init_db',
+    'close_db'
+]
+
+
+logger = logging.getLogger(__name__)
+
+
+def init_db(app: FastAPI) -> None:
+    app.state.db_engine = create_engine(sqlalchemy_connection_string)
+    app.state.db_sessionmaker = create_sessionmaker(app.state.db_engine)
+    logger.info('Database engine and sessionmaker have been set.')
+
+
+async def close_db(app: FastAPI) -> None:
+    await app.state.db_engine.dispose()
+    logger.info('Database engine has been disposed.')

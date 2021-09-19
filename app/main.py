b59cecc348cbcbcc8413149592806bb3cd0998bc
Max Zhenzhera <59729293+Max-Zhenzhera@users.noreply.github.com>
2021-09-19 14:15:48 +0300

    add event handlers

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 
+from app.api.routes import router
 from app.core.config import (
     server_config,
     uvicorn_config
 )
-from app.api.routes import router
+from app.core.events import (
+    get_startup_handler,
+    get_shutdown_handler
+)
 
 
 def get_application() -> FastAPI:
     application = FastAPI()
+
+    application.add_event_handler('startup', get_startup_handler(application))
+    application.add_event_handler('shutdown', get_shutdown_handler(application))
+
     application.include_router(router, prefix=server_config.API_PREFIX)
+
     return application
 
 

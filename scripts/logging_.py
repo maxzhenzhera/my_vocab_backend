a8f737305c89465eada6aa8dd49dc6e9cb1a64dd
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-03 09:23:13 +0300

    add helper scripts for preparing environment

---
+++ scripts/logging_.py
@@ -0,0 +1,14 @@
+import logging
+
+
+__all__ = ['logger']
+
+
+DEFAULT_LOGGING_CONFIG = {
+    "format": "%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s() [%(lineno)s] : %(message)s",
+    "level": logging.INFO
+}
+
+
+logging.basicConfig(**DEFAULT_LOGGING_CONFIG)
+logger = logging.getLogger('scripts')

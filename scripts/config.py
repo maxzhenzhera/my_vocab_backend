52ce174fb8f34fee8a83e27c1a282332626c7c17
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-03 11:27:15 +0300

    use logging_config_path from app config

--- scripts/config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-__all__ = ['LOGGING_CONFIG_PATH']
-
-
-SCRIPTS_DIR = Path(__file__).parent
-BASE_DIR = SCRIPTS_DIR.parent
-LOGGING_CONFIG_PATH = BASE_DIR / 'app' / 'utils' / 'logging_' / 'logging_config.yaml'

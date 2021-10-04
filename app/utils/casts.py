f90294f7636eb096acfc2b6b2e64bbddc85cdeb0
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-04 21:05:29 +0300

    refactor to_bool function

--- app/utils/casts.py
+++ app/utils/casts.py
@@ -8,4 +8,4 @@ TRUE_STRINGS = {'true', 't', 'yes', '1'}
 
 
 def to_bool(string: Optional[str]) -> bool:
-    return False if not string else string.lower() in TRUE_STRINGS
+    return False if string is None else string.lower() in TRUE_STRINGS

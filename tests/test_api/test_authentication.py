eae52939ca8ec1d04aec86b522a2036f887cd0bd
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-04 22:15:12 +0300

    refactor tests for registration route

--- tests/test_api/test_authentication.py
+++ tests/test_api/test_authentication.py
@@ -1,59 +1,46 @@
 import pytest
-from fastapi import FastAPI
-from httpx import AsyncClient
+from httpx import AsyncClient, Response
 
-from app.core.config import mail_connection_config
+from app.main import app
 from app.schemas.user import UserInCreate
+from tests.config import mail_connection_test_config
 
 
 pytestmark = pytest.mark.asyncio
 
 
-async def test_register_route_result(app: FastAPI, test_client: AsyncClient):
-    response = await test_client.post(
-        app.url_path_for('auth:register'),
-        json=UserInCreate(email='example@gmail.com', password='password').dict()  # noqa
-    )
-    response_json = response.json()
+class TestRegisterRoute:
+    URL = app.url_path_for('auth:register')
+    TEST_USER = UserInCreate(email='example@gmail.com', password='password')    # noqa pydantic email validation
+    TEST_JSON = TEST_USER.dict()
 
-    assert response.status_code == 200
-    assert 'user' in response_json and 'tokens' in response_json
+    @pytest.fixture(name='response')
+    async def response_from_route(self, test_client: AsyncClient) -> Response:
+        return await test_client.post(self.URL, json=self.TEST_JSON)
 
-
-async def test_register_route_refresh_token_cookie(app: FastAPI, test_client: AsyncClient):
-    response = await test_client.post(
-        app.url_path_for('auth:register'),
-        json=UserInCreate(email='example@gmail.com', password='password').dict()  # noqa
-    )
-
-    assert response.status_code == 200
-    assert 'refresh_token' in response.cookies
-
-
-async def test_register_route_email_sending(app: FastAPI, test_client: AsyncClient):
-    with test_client._transport.app.state.mail_sender.record_messages() as outbox:
-        response = await test_client.post(
-            app.url_path_for('auth:register'),
-            json=UserInCreate(email='example@gmail.com', password='password').dict()  # noqa
-        )
+    async def test_route_result(self, response: Response):
+        response_json = response.json()
 
         assert response.status_code == 200
+        assert 'user' in response_json and 'tokens' in response_json
 
-        assert len(outbox) == 1
-        if mail_connection_config.MAIL_FROM_NAME is not None:
-            assert outbox[0]['from'] == f"{mail_connection_config.MAIL_FROM_NAME} <{mail_connection_config.MAIL_FROM}>"
-        else:
-            assert outbox[0]['from'] == mail_connection_config.MAIL_FROM
-        assert outbox[0]['To'] == "example@gmail.com"
+    async def test_route_setting_refresh_token_cookie(self, response: Response):
+        assert 'refresh_token' in response.cookies
 
+    async def test_register_route_email_sending(self, test_client: AsyncClient):
+        with test_client._transport.app.state.mail_sender.record_messages() as outbox:
+            _ = await test_client.post(self.URL, json=self.TEST_JSON)
 
-async def test_register_route_raise_400(app: FastAPI, test_client: AsyncClient):
-    _ = await test_client.post(
-        app.url_path_for('auth:register'),
-        json=UserInCreate(email='example@gmail.com', password='password').dict()  # noqa
-    )
-    response = await test_client.post(
-        app.url_path_for('auth:register'),
-        json=UserInCreate(email='example@gmail.com', password='password').dict()  # noqa
-    )
-    assert response.status_code == 400
+            assert len(outbox) == 1
+            if mail_connection_test_config.MAIL_FROM_NAME is not None:
+                from_ = f"{mail_connection_test_config.MAIL_FROM_NAME} <{mail_connection_test_config.MAIL_FROM}>"
+                assert outbox[0]['from'] == from_
+            else:
+                from_ = mail_connection_test_config.MAIL_FROM
+                assert outbox[0]['from'] == from_
+            assert outbox[0]['To'] == self.TEST_USER.email
+
+    async def test_route_return_400_error(self, response: Response, test_client: AsyncClient):
+        """ On registration has been passed the same credentials (used fixture) - must return 400 Bad Request """
+        response = await test_client.post(self.URL, json=self.TEST_JSON)
+        assert response.status_code == 400

27ff31d8f52762ff05578197740e9b67878dce6c
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-04 21:08:48 +0300

    refactor and clean

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,14 @@
 import pytest
 from fastapi import FastAPI
-from fastapi_mail import FastMail
 from httpx import AsyncClient
 
-from app.core.config import mail_connection_config
-from app.db.models import Base
-from app.db.postgres import (
-    create_engine,
-    create_sessionmaker
+from app.db.events import _set_db_in_app                        # noqa ; not in __all__; access to a protected member
+from app.services.mail.events import _set_mail_sender_in_app    # noqa ; not in __all__; access to a protected member
+from tests.config import (
+    sqlalchemy_connection_string_to_test_database,
+    mail_connection_test_config
 )
-from tests.env import sqlalchemy_connection_string_to_test_database
+from tests.helpers.db import create_clear_db
 
 
 @pytest.fixture(name='app')
@@ -21,17 +20,9 @@ def fixture_app() -> FastAPI:
 
 @pytest.fixture(name='test_app')
 async def fixture_test_app(app: FastAPI) -> FastAPI:
-    # set mail sender config for suppress sending
-    app.state.mail_sender = FastMail(mail_connection_config)
-    app.state.mail_sender.config.SUPPRESS_SEND = 1
-    # put connection to test database
-    app.state.db_engine = create_engine(sqlalchemy_connection_string_to_test_database)
-    app.state.db_sessionmaker = create_sessionmaker(app.state.db_engine)
-    # drop and create tables in test database
-    async with app.state.db_engine.begin() as connection:
-        await connection.run_sync(Base.metadata.drop_all)
-        await connection.run_sync(Base.metadata.create_all)
-
+    _set_mail_sender_in_app(mail_connection_test_config, app)
+    _set_db_in_app(sqlalchemy_connection_string_to_test_database, app)
+    await create_clear_db(app)
     return app
 
 
@@ -39,7 +30,7 @@ async def fixture_test_app(app: FastAPI) -> FastAPI:
 async def fixture_client(test_app: FastAPI) -> AsyncClient:
     async with AsyncClient(
         app=test_app,
-        base_url=f"http://testserver",   # noqa
+        base_url=f"http://testserver",
         headers={"Content-Type": "application/json"},
     ) as client:
         yield client

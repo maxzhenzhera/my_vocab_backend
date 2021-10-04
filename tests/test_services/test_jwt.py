fc62f8f7158c8367041ba5ecfe8e1d0a8b9489eb
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-04 21:50:16 +0300

    take out tests for decode_token helper function in new class; refactor

--- tests/test_services/test_jwt.py
+++ tests/test_services/test_jwt.py
@@ -7,7 +7,7 @@ from time import sleep
 import pytest
 from jose import jwt
 
-from app.core.config import jwt_config
+from app.core.config.config import jwt_config
 from app.db.models import User
 from app.schemas.jwt import (
     JWTMeta,
@@ -40,46 +40,13 @@ class TestUserJWTService:
     def fixture_service(self, user: User) -> UserJWTService:
         return UserJWTService(user)
 
+    def test_jwt_user(self, service: UserJWTService):
+        assert JWTUser.from_orm(service.user) == service.jwt_user
+
     def test_tokens_verifying(self, service: UserJWTService):
         tokens = service.generate_tokens()
         assert isinstance(service.verify_access_token(tokens.access_token.token), JWTUser)
         assert isinstance(service.verify_refresh_token(tokens.refresh_token.token), JWTUser)
-
-    def test_decode_token(self, service: UserJWTService):
-        common_test_payload = {
-            'sub': 'test',
-            'data': 'data'
-        }
-        test_token_expire_in_seconds = 1
-        test_payload_for_expire = {
-            'exp': service._compute_expire(timedelta(seconds=test_token_expire_in_seconds)),
-            **common_test_payload
-        }
-        test_payload_for_invalid_signature = {
-            'exp': service._compute_expire(timedelta(hours=1)),
-            **common_test_payload
-        }
-
-        test_token_for_expire = jwt.encode(test_payload_for_expire, self.TEST_TOKEN_SECRET, jwt_config.ALGORITHM)
-        with pytest.raises(ExpiredTokenSignatureError):
-            sleep(test_token_expire_in_seconds + 1)
-            decode_token(TokenDataForDecoding(test_token_for_expire, self.TEST_TOKEN_SECRET))
-
-        test_token_for_invalid_signature = jwt.encode(
-            test_payload_for_invalid_signature, self.TEST_TOKEN_SECRET, jwt_config.ALGORITHM
-        )
-        with pytest.raises(InvalidTokenSignatureError):
-            test_token_with_injection = test_token_for_invalid_signature + 'injection'
-            decode_token(TokenDataForDecoding(test_token_with_injection, self.TEST_TOKEN_SECRET))
-        with pytest.raises(InvalidTokenSignatureError):
-            test_token_invalid_secret = self.TEST_TOKEN_SECRET + 'invalid'
-            decode_token(TokenDataForDecoding(test_token_for_invalid_signature, test_token_invalid_secret))
-
-        normal_result = decode_token(TokenDataForDecoding(test_token_for_invalid_signature, self.TEST_TOKEN_SECRET))
-        assert isinstance(normal_result, dict)
-
-    def test_jwt_user(self, service: UserJWTService):
-        assert JWTUser.from_orm(service.user) == service.jwt_user
 
     def test_generate_tokens(self, service: UserJWTService):
         assert isinstance(service.generate_tokens(), Tokens)
@@ -90,11 +57,9 @@ class TestUserJWTService:
         manual_token = Token(
             jwt.encode(test_payload, self.TEST_TOKEN_SECRET, jwt_config.ALGORITHM), expires_at
         )
-
         service_token = service._generate_token(TokenDataForEncoding(self.TEST_META_CLAIMS, self.TEST_TOKEN_SECRET))
-
         assert isinstance(service_token.expires_at, datetime)
-        # GitHub Actions time difference on computing
+        # GitHub Actions time difference on computing -> compare only by <token> field
         assert manual_token.token == service_token.token
 
     def test_prepare_payload(self, service: UserJWTService):
@@ -118,3 +83,41 @@ class TestUserJWTService:
         service_expire = UserJWTService._compute_expire(test_delta)
         manual_expire = datetime.utcnow() + test_delta
         assert (manual_expire - service_expire) < computational_interference
+
+
+class TestJWTHelperDecodeToken:
+    TEST_TOKEN_SECRET = 'secret'
+    COMMON_TEST_PAYLOAD = {
+        'sub': 'test',
+        'data': 'data'
+    }
+    TEST_PAYLOAD = {
+        'exp': UserJWTService._compute_expire(timedelta(hours=1)),
+        **COMMON_TEST_PAYLOAD
+    }
+    TEST_TOKEN = jwt.encode(TEST_PAYLOAD, TEST_TOKEN_SECRET, jwt_config.ALGORITHM)
+
+    def test_decode_token_on_expire(self):
+        expire_in_seconds = 1
+        test_payload = {
+            'exp': UserJWTService._compute_expire(timedelta(seconds=expire_in_seconds)),
+            **self.COMMON_TEST_PAYLOAD
+        }
+        test_token = jwt.encode(test_payload, self.TEST_TOKEN_SECRET, jwt_config.ALGORITHM)
+        with pytest.raises(ExpiredTokenSignatureError):
+            sleep(expire_in_seconds + 1)
+            decode_token(TokenDataForDecoding(test_token, self.TEST_TOKEN_SECRET))
+
+    def test_decode_token_on_injection(self):
+        with pytest.raises(InvalidTokenSignatureError):
+            test_token_with_injection = self.TEST_TOKEN + 'injection'
+            decode_token(TokenDataForDecoding(test_token_with_injection, self.TEST_TOKEN_SECRET))
+
+    def test_decode_token_on_invalid_secret(self):
+        with pytest.raises(InvalidTokenSignatureError):
+            test_token_invalid_secret = self.TEST_TOKEN_SECRET + 'invalid'
+            decode_token(TokenDataForDecoding(self.TEST_TOKEN, test_token_invalid_secret))
+
+    def test_decode_token_on_normal_conditions(self):
+        normal_result = decode_token(TokenDataForDecoding(self.TEST_TOKEN, self.TEST_TOKEN_SECRET))
+        assert isinstance(normal_result, dict)

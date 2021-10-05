631e2fb8dbfebb98da920041ee55c8a45556f97a
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-05 20:20:31 +0300

    add main script that prepares project environment

---
+++ scripts/prepare.py
@@ -0,0 +1,14 @@
+from app.core.config.paths import LOGGING_CONFIG_PATH
+from scripts.logging_ import logger
+from scripts.make_directories_for_logs import make_directories_for_logs
+
+
+def main():
+    logger.info('Run script for preparing project environment.')
+
+    logger.info('Creating directories for logs...')
+    make_directories_for_logs(LOGGING_CONFIG_PATH)
+
+
+if __name__ == '__main__':
+    main()

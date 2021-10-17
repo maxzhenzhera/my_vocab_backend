27efb0b65f666686d5103004c768387d1619ff57
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-17 13:27:33 +0300

    add <create> and <login> routes; update docs for the each route

--- app/api/routes/endpoints/authentication.py
+++ app/api/routes/endpoints/authentication.py
@@ -5,15 +5,25 @@ from fastapi import (
     Depends,
     HTTPException
 )
-from starlette.status import HTTP_400_BAD_REQUEST
+from starlette.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED
+)
 
 from ....schemas.authentication import AuthenticationResult
-from ....schemas.user import UserInCreate
+from ....schemas.user import (
+    UserInCreate,
+    UserInLogin,
+    UserInResponse
+)
 from ....services.authentication import (
     AuthenticationService,
     CookieService
 )
-from ....services.authentication.errors import RegistrationError
+from ....services.authentication.errors import (
+    AuthenticationError,
+    RegistrationError
+)
 from ....services.mail import MailService
 
 
@@ -25,6 +35,38 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
+@router.post('/create', response_model=UserInResponse, name='auth:create')
+async def create(
+        user_in_create: UserInCreate,
+        authentication_service: AuthenticationService = Depends()
+):
+    """
+    Create a user by the given credentials.
+    Route just creates a user in db. Mostly aims at the test fixtures or manual testing.
+
+    Arguments
+    ---------
+        < UserInCreate > model
+
+    Return
+    ---------
+        * Body
+            < AuthenticationResult > model
+
+    Raise
+    ---------
+        * 400 BAD REQUEST
+            The given credentials are invalid or (most likely) already used before.
+    """
+
+    try:
+        user = await authentication_service.create_user(user_in_create)
+    except RegistrationError as error:
+        raise HTTPException(HTTP_400_BAD_REQUEST, error.detail)
+    else:
+        return user
+
+
 @router.post('/register', response_model=AuthenticationResult, name='auth:register')
 async def register(
         user_in_create: UserInCreate,
@@ -33,7 +75,8 @@ async def register(
         mail_service: MailService = Depends()
 ):
     """
-    Register an user with the given credentials.
+    Register a user by the given credentials.
+    Route creates a user and logins him simultaneously.
 
     Arguments
     ---------
@@ -45,6 +88,16 @@ async def register(
             < AuthenticationResult > model
         * Cookies
             < refresh_token> httpOnly cookie
+
+    Raise
+    ---------
+        * 400 BAD REQUEST
+            The given credentials are invalid or (most likely) already used before.
+
+    Other
+    ---------
+        * Email
+            Send confirmation mail to indicated email.
     """
 
     try:
@@ -54,4 +107,39 @@ async def register(
     else:
         cookie_service.set_refresh_token(authentication_result.tokens.refresh_token)
         mail_service.send_confirmation_mail(authentication_result.user)
+        return authentication_result
+
+
+@router.post('/login', response_model=AuthenticationResult, name='auth:login')
+async def login(
+        user_in_login: UserInLogin,
+        authentication_service: AuthenticationService = Depends(),
+        cookie_service: CookieService = Depends()
+):
+    """
+    Login a user by the given credentials.
+
+    Arguments
+    ---------
+        < UserInLogin > model
+
+    Return
+    ---------
+        * Body
+            < AuthenticationResult > model
+        * Cookies
+            < refresh_token> httpOnly cookie
+
+    Raise
+    ---------
+        * 401 UNAUTHORIZED
+            The incorrect credentials.
+    """
+
+    try:
+        authentication_result = await authentication_service.login(user_in_login)
+    except AuthenticationError as error:
+        raise HTTPException(HTTP_401_UNAUTHORIZED, error.detail)
+    else:
+        cookie_service.set_refresh_token(authentication_result.tokens.refresh_token)
         return authentication_result

643e4b0d08144b869f26d5d290da5134fa46ac0c
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-22 11:31:21 +0300

    add import of the UserAccountService

--- app/services/authentication/__init__.py
+++ app/services/authentication/__init__.py
@@ -1,8 +1,10 @@
 from .authentication import AuthenticationService
 from .cookie import CookieService
+from .user_account import UserAccountService
 
 
 __all__ = [
     'AuthenticationService',
-    'CookieService'
+    'CookieService',
+    'UserAccountService'
 ]

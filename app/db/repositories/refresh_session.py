8db20dfafcdc3df5fb18ff2af0d9830d1540d57b
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-22 22:27:29 +0300

    implement method for deleting refresh session by refresh token

--- app/db/repositories/refresh_session.py
+++ app/db/repositories/refresh_session.py
@@ -1,3 +1,4 @@
+from sqlalchemy import delete as sa_delete
 from sqlalchemy.future import select as sa_select
 
 from .base import BaseRepository
@@ -13,3 +14,8 @@ class RefreshSessionsRepository(BaseRepository):
     async def fetch_by_refresh_token(self, refresh_token: str) -> RefreshSession:
         stmt = sa_select(RefreshSession).where(RefreshSession.refresh_token == refresh_token)
         return await self._fetch_entity(stmt)
+
+    async def delete_by_refresh_token(self, refresh_token: str) -> None:
+        async with self._session.begin_nested():
+            stmt = sa_delete(RefreshSession).where(RefreshSession.refresh_token == refresh_token)
+            await self._session.execute(stmt)

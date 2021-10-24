3f967b7db18f0787316e41cc7afbb1e4de2cae62
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-24 17:37:33 +0300

    delete tokens schemas

--- app/schemas/jwt.py
+++ app/schemas/jwt.py
@@ -3,15 +3,11 @@ from datetime import datetime
 from pydantic import BaseModel
 
 from .base import ModelWithOrmMode
-from ..core.config.config import jwt_config
 
 
 __all__ = [
     'JWTMeta',
-    'JWTUser',
-    'AccessTokenInResponse',
-    'RefreshTokenInResponse',
-    'TokensInResponse'
+    'JWTUser'
 ]
 
 
@@ -23,22 +19,3 @@ class JWTMeta(BaseModel):
 class JWTUser(ModelWithOrmMode):
     email: str
     is_superuser: bool
-
-
-class TokenInResponse(ModelWithOrmMode):
-    token: str
-    token_type: str
-    expires_at: datetime
-
-
-class AccessTokenInResponse(TokenInResponse):
-    token_type: str = jwt_config.ACCESS_TOKEN_TYPE
-
-
-class RefreshTokenInResponse(TokenInResponse):
-    token_type: str = jwt_config.REFRESH_TOKEN_TYPE
-
-
-class TokensInResponse(ModelWithOrmMode):
-    access_token: AccessTokenInResponse
-    refresh_token: RefreshTokenInResponse

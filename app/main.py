27aab1df18cba8397a4c573262cd4ff80445efd3
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-10-24 17:30:17 +0300

    use updated uvicorn config

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.routes import router
-from app.core.config.config import (
-    server_config,
-    uvicorn_config
-)
+from app.core.config.config import uvicorn_config
 from app.core.events import (
     get_startup_handler,
     get_shutdown_handler
@@ -20,7 +17,7 @@ def get_application() -> FastAPI:
     application.add_event_handler('startup', get_startup_handler(application))
     application.add_event_handler('shutdown', get_shutdown_handler(application))
 
-    application.include_router(router, prefix=server_config.API_PREFIX)
+    application.include_router(router, prefix=uvicorn_config.SERVER_CONFIG.API_PREFIX)
 
     return application
 
@@ -31,4 +28,4 @@ app = get_application()
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('main:app', **uvicorn_config)
+    uvicorn.run('main:app', **uvicorn_config.get_run_config())

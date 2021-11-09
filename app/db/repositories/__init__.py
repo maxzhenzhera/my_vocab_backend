1a699d9d8aed74c460c66e5d071b6cfbdfe6511e
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-09 23:00:31 +0200

    add import for oauth connections repository

--- app/db/repositories/__init__.py
+++ app/db/repositories/__init__.py
@@ -1,10 +1,12 @@
 from .base import BaseRepository
 from .refresh_session import RefreshSessionsRepository
 from .user import UsersRepository
+from .oauth_connections import OAuthConnectionsRepository
 
 
 __all__ = [
     'BaseRepository',
     'RefreshSessionsRepository',
-    'UsersRepository'
+    'UsersRepository',
+    'OAuthConnectionsRepository'
 ]

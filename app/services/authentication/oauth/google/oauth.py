9c69d99d72c476573840e4c9e21ee4c84ee5e7ee
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-09 23:28:18 +0200

    add google oauth authentication service

---
+++ app/services/authentication/oauth/google/oauth.py
@@ -0,0 +1,21 @@
+from ..base.oauth import BaseOAuthService
+
+from .....db.models import (
+    User,
+    OAuthConnection
+)
+from .....schemas.authentication.oauth import (
+    GoogleOAuthConnection,
+    OAuthUser
+)
+
+
+__all__ = ['GoogleOAuthService']
+
+
+class GoogleOAuthService(BaseOAuthService):
+    async def _fetch_oauth_connection(self, oauth_user: OAuthUser) -> OAuthConnection:
+        return await self.oauth_connections_repository.fetch_by_google_id_with_user(oauth_user.id)
+
+    def _build_oauth_connection_instance(self, oauth_user: OAuthUser, internal_user: User) -> GoogleOAuthConnection:
+        return GoogleOAuthConnection(user_id=internal_user.id, google_id=oauth_user.id)

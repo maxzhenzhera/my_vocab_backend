38a888b5988e60129a5eb75d93414424a32a8f31
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-09 23:16:54 +0200

    fix config import; update refresh token max-age method

--- app/services/authentication/cookie.py
+++ app/services/authentication/cookie.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
-from datetime import timedelta
 from pathlib import PurePosixPath as URLPathJoiner
 
 from fastapi import Response
 
-from ...core.config.config import (
+from ...core.config import (
     server_config,
     refresh_session_config
 )
@@ -29,11 +28,7 @@ class CookieService:
 
     @property
     def refresh_token_cookie_max_age(self) -> int:
-        return self._get_total_seconds_of_timedelta(refresh_session_config.REFRESH_TOKEN_EXPIRE_TIMEDELTA)
-
-    @staticmethod
-    def _get_total_seconds_of_timedelta(delta: timedelta) -> int:
-        return int(delta.total_seconds())
+        return int(refresh_session_config.REFRESH_TOKEN_EXPIRE_TIMEDELTA.total_seconds())
 
     def set_refresh_token(self, refresh_token: str) -> None:
         self.response.set_cookie(

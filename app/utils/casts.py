0b93e84bec9d8a4aa947f73922cba19578600804
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-09 23:02:43 +0200

    add raising of ValueError on invalid argument

--- app/utils/casts.py
+++ app/utils/casts.py
@@ -5,7 +5,18 @@ __all__ = ['to_bool']
 
 
 TRUE_STRINGS = {'true', 't', 'yes', '1'}
+FALSE_STRINGS = {'false', 'f', 'no', 'not', '0'}
 
 
 def to_bool(string: Optional[str]) -> bool:
-    return False if string is None else string.lower() in TRUE_STRINGS
+    if string is None:
+        return False
+    if string.lower() in TRUE_STRINGS:
+        return True
+    if string.lower() in FALSE_STRINGS:
+        return False
+    raise ValueError(
+        f'Expected to get the optional string to cast. Got {string!r}.\n'
+        f'Possible true values: {TRUE_STRINGS}.\n'
+        f'Possible false values: {FALSE_STRINGS}.\n'
+    )

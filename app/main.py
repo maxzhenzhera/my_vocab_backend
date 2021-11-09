98b091f65a4915f1ccce2ff57a3bf044a6c28c7c
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-09 23:32:31 +0200

    add middlewares: session, CORS; refactor

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 from app.api.routes import router
-from app.core.config.config import uvicorn_config
+from app.core.config import (
+    CORS_ORIGINS,
+    SESSION_MIDDLEWARE_SECRET_KEY,
+    uvicorn_config
+)
 from app.core.events import (
     get_startup_handler,
     get_shutdown_handler
@@ -16,6 +22,18 @@ def get_application() -> FastAPI:
 
     application.add_event_handler('startup', get_startup_handler(application))
     application.add_event_handler('shutdown', get_shutdown_handler(application))
+
+    application.add_middleware(
+        SessionMiddleware,
+        secret_key=SESSION_MIDDLEWARE_SECRET_KEY
+    )
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     application.include_router(router, prefix=uvicorn_config.SERVER_CONFIG.API_PREFIX)
 

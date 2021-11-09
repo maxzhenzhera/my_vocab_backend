1efcc48218b05f881ed23d0d0b1005e1e018bf80
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-09 23:31:27 +0200

    include oauth router

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .endpoints.authentication import router as authentication_router
+from .endpoints.oauth import router as oauth_router
 
 
 __all__ = ['router']
@@ -9,3 +10,4 @@ __all__ = ['router']
 router = APIRouter()
 
 router.include_router(authentication_router, tags=['auth'], prefix='/auth')
+router.include_router(oauth_router, tags=['oauth'], prefix='/oauth')

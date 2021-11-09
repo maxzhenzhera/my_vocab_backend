32debab06e3dc6d538396d7c65fc0263efc164f5
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-09 22:46:55 +0200

    add model for oauth connections

---
+++ app/db/models/auth/oauth_connection.py
@@ -0,0 +1,25 @@
+from sqlalchemy import (
+    BigInteger,
+    Column,
+    ForeignKey,
+    String
+)
+from sqlalchemy.orm import relationship
+
+from ..base import Base
+from ...constants import CASCADE
+
+
+__all__ = ['OAuthConnection']
+
+
+class OAuthConnection(Base):
+    __tablename__ = 'oauth_connections'
+
+    user_id = Column(BigInteger, ForeignKey('users.id', ondelete=CASCADE), primary_key=True, autoincrement=False)
+    google_id = Column(String(64), unique=True)
+
+    user = relationship('User', back_populates='oauth_connection')
+
+    def __repr__(self) -> str:
+        return f'OAuthConnection(user_id={self.user_id!r}, google_id={self.google_id!r})'

fdc53cee43a9784bc9da8aaf96f6e7de660f9207
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-17 09:42:46 +0200

    simplify db '_get_session' dependency

--- app/api/dependencies/db.py
+++ app/api/dependencies/db.py
@@ -8,7 +8,6 @@ from fastapi import (
     Request
 )
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import sessionmaker
 
 from ...db.repositories import BaseRepository
 
@@ -16,16 +15,12 @@ from ...db.repositories import BaseRepository
 __all__ = ['get_repository']
 
 
-def _get_sessionmaker(request: Request) -> sessionmaker:
-    return request.app.state.db_sessionmaker
-
-
-async def _get_session(db_sessionmaker: sessionmaker = Depends(_get_sessionmaker)) -> AsyncSession:
-    async with db_sessionmaker() as session, session.begin():
-        yield session
-
-
 def get_repository(repository_type: Type[BaseRepository]) -> Callable[[AsyncSession], BaseRepository]:
     def _get_repo(session: AsyncSession = Depends(_get_session)) -> BaseRepository:
         return repository_type(session)
     return _get_repo
+
+
+async def _get_session(request: Request) -> AsyncSession:
+    async with request.app.state.db_sessionmaker() as session, session.begin():
+        yield session

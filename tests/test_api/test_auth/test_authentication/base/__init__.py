9e2f802e56e3e1a7dbcf67c7970c97ac3b68d320
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-29 10:33:49 +0200

    add __init__ for package

---
+++ tests/test_api/test_auth/test_authentication/base/__init__.py
@@ -0,0 +1,8 @@
+from .terminating_refresh_session_route import BaseTestTerminatingRefreshSessionRoute
+from .user_creation_route import BaseTestUserCreationRoute
+
+
+__all__ = [
+    'BaseTestTerminatingRefreshSessionRoute',
+    'BaseTestUserCreationRoute'
+]

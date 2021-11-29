569d5267c1ec2dd7d54460cc931dd0c4da1ea13b
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-29 10:58:46 +0200

    add tests for google signup

---
+++ tests/test_api/test_auth/test_oauth/test_google/test_signup.py
@@ -0,0 +1,15 @@
+from typing import ClassVar
+
+import pytest
+from starlette.datastructures import URLPath
+
+from app.main import app
+from ..base import BaseTestRedirectToOauthRoute
+
+
+pytestmark = pytest.mark.asyncio
+
+
+class TestGoogleSignupRoute(BaseTestRedirectToOauthRoute):
+    url: ClassVar[URLPath] = app.url_path_for('oauth:google:signup')
+    redirect_location_domain: ClassVar[str] = 'google.com'

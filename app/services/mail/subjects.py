a7cb6b57dd30b7ccb9a9efdaa3fef4e9246b02a6
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-29 10:03:58 +0200

    add module with email subjects

---
+++ app/services/mail/subjects.py
@@ -0,0 +1,8 @@
+__all__ = [
+    'CONFIRMATION_MAIL_SUBJECT',
+    'CREDENTIALS_MAIL_SUBJECT'
+]
+
+
+CONFIRMATION_MAIL_SUBJECT = 'Account email confirmation'
+CREDENTIALS_MAIL_SUBJECT = 'Account credentials'

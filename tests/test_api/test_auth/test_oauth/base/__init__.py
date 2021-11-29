2edff8ee82a62ceb86f82f1c15b32a403f1435d6
Max Zhenzhera <maxzhenzhera@gmail.com>
2021-11-29 10:57:41 +0200

    add __init__ for package

---
+++ tests/test_api/test_auth/test_oauth/base/__init__.py
@@ -0,0 +1,10 @@
+from .oauth_login_route import BaseOauthLoginRoute
+from .oauth_register_route import BaseOauthRegisterRoute
+from .redirect_to_oauth_route import BaseTestRedirectToOauthRoute
+
+
+__all__ = [
+    'BaseOauthLoginRoute',
+    'BaseOauthRegisterRoute',
+    'BaseTestRedirectToOauthRoute'
+]

9432c36a5c5e37b702a3c13f06a8e198e3f0438d
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-22 19:25:28 +0200

    feat: add updated event handlers

--- app/core/events.py
+++ app/core/events.py
@@ -1,12 +1,21 @@
-from typing import Callable
+from collections.abc import (
+    Callable,
+    Coroutine
+)
+from typing import (
+    Any,
+    TypeAlias
+)
 
 from fastapi import FastAPI
 
+from .settings import AppSettings
 from ..db.events import (
-    init_db,
-    close_db
+    close_db,
+    init_db
 )
-from ..services.mail.events import init_mail_sender
+from ..services.authentication.oauth.events import init_oauth
+from ..services.mail.events import init_mail
 
 
 __all__ = [
@@ -15,15 +24,19 @@ __all__ = [
 ]
 
 
-def get_startup_handler(app: FastAPI) -> Callable:
+EventHandler: TypeAlias = Callable[[], Coroutine[Any, Any, None]]
+
+
+def get_startup_handler(app: FastAPI, settings: AppSettings) -> EventHandler:
     async def start_app() -> None:
-        init_db(app)
-        init_mail_sender(app)
+        init_db(app, settings.db)
+        init_mail(app, settings.mail)
+        init_oauth(app, settings.oauth)
 
     return start_app
 
 
-def get_shutdown_handler(app: FastAPI) -> Callable:
+def get_shutdown_handler(app: FastAPI) -> EventHandler:
     async def shutdown_app() -> None:
         await close_db(app)
 

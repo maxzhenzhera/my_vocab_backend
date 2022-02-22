25fd7ceb3f5517e536ba4fe7672330cc9ed7999f
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-22 22:41:50 +0200

    refactor: use python3.10 Union, delete explicit type annotation for level attr

--- app/utils/logging_/filters.py
+++ app/utils/logging_/filters.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Union
+from logging import LogRecord
 
 
 __all__ = ['LevelFilter']
@@ -8,18 +8,18 @@ __all__ = ['LevelFilter']
 class LevelFilter(logging.Filter):
     """ Filters log records by level (includes logs only by one level). """
 
-    def __init__(self, level: Union[int, str]):
+    def __init__(self, level: int | str) -> None:
         super().__init__()
 
         if isinstance(level, str):
-            self.level: int = getattr(logging, level.upper())
+            self.level = getattr(logging, level.upper())
         elif isinstance(level, int):
-            self.level: int = level
+            self.level = level
         else:
             raise ValueError(
                 'Expected to get either levelno [int] either levelname [str]. '
                 f'Instead got {level!r}.'
             )
 
-    def filter(self, record: logging.LogRecord) -> bool:
+    def filter(self, record: LogRecord) -> bool:
         return record.levelno == self.level

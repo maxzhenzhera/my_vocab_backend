b56ac089c728790c4a6e76371c6572a0d2d6ec41
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-22 19:59:52 +0200

    feat: add paths

--- app/core/settings/paths.py
+++ app/core/settings/paths.py
@@ -4,12 +4,15 @@ from pathlib import Path
 __alL__ = [
     'APP_DIR',
     'BASE_DIR',
-    'LOGGING_CONFIG_PATH',
-    'EMAIL_TEMPLATES_DIR'
+    'EMAIL_TEMPLATES_DIR',
+    'ALEMBIC_CONFIG_PATH',
+    'LOGGING_CONFIG_PATH'
 ]
 
 
-APP_DIR = Path(__file__).parent.parent
+APP_DIR = Path(__file__).parent.parent.parent
 BASE_DIR = APP_DIR.parent
-LOGGING_CONFIG_PATH = APP_DIR / 'utils' / 'logging_' / 'logging_config.yaml'
 EMAIL_TEMPLATES_DIR = APP_DIR / 'email_templates'
+
+ALEMBIC_CONFIG_PATH = BASE_DIR / 'alembic.ini'
+LOGGING_CONFIG_PATH = BASE_DIR / 'configs' / 'logging.yaml'

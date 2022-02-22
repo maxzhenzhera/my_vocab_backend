fbb5ae109396c5a89e30cfcf74d0a5ffb7431ede
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-22 20:24:09 +0200

    feat: improve uuid db default

--- app/db/functions/server_defaults/uuid_.py
+++ app/db/functions/server_defaults/uuid_.py
@@ -1,16 +1,27 @@
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.sql import expression
+from sqlalchemy.sql.expression import FunctionElement  # type: ignore[attr-defined]
 
 
 __all__ = ['gen_random_uuid']
 
 
-class gen_random_uuid(expression.FunctionElement):       # noqa Class names should use CamelCase convention
+class GenRandomUUID(FunctionElement):  # type: ignore[misc]
     type = UUID()
 
 
-@compiles(gen_random_uuid, 'postgresql')
-def pg_gen_random_uuid(element, compiler, **kw):
-    """ postgres docs: https://www.postgresql.org/docs/14/functions-uuid.html"""
+@compiles(GenRandomUUID, 'postgresql')  # type: ignore[misc]
+def pg_gen_random_uuid(element, compiler, **kw) -> str:  # type: ignore[no-untyped-def]
+    """
+    Postgres docs:
+    https://www.postgresql.org/docs/14/functions-uuid.html
+    """
+
     return 'gen_random_uuid()'
+
+
+# Classes should be named by CamelCase.
+# Functions should be named by snake_case.
+# Since class represents `FunctionElement` (also means might be used as a function)
+# lowercase alias has been created.
+gen_random_uuid = GenRandomUUID

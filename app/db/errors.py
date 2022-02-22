a2db128ff9118495bdb14a16c7a883e88a0de22e
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-22 21:38:24 +0200

    refactor: delete unused exceptions

--- app/db/errors.py
+++ app/db/errors.py
@@ -1,16 +1,11 @@
 """
 DBError
     +-- EntityDoesNotExistError
-    +-- UserUpdateError
-        +-- EmailInUpdateIsAlreadyTakenError
 """
-
 
 __all__ = [
     'DBError',
-    'EntityDoesNotExistError',
-    'UserUpdateError',
-    'EmailInUpdateIsAlreadyTakenError'
+    'EntityDoesNotExistError'
 ]
 
 
@@ -20,22 +15,3 @@ class DBError(Exception):
 
 class EntityDoesNotExistError(DBError):
     """ Raised if the searched entity does not exist in the database. """
-
-
-class UserUpdateError(DBError):
-    """ Common user update exception. """
-
-    @property
-    def detail(self) -> str:
-        return 'Given data is invalid for user update.'
-
-
-class EmailInUpdateIsAlreadyTakenError(UserUpdateError):
-    """ Raised on the user update if the email is already used by the other user. """
-
-    def __init__(self, email: str) -> None:
-        self.email = email
-
-    @property
-    def detail(self) -> str:
-        return f"Email '{self.email}' is busy. Please, use another email!"

88c4bb30419d15bd96190a131caebd9583626b9e
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-22 20:23:27 +0200

    feat: improve utcnow db default

--- app/db/functions/server_defaults/utcnow.py
+++ app/db/functions/server_defaults/utcnow.py
@@ -1,16 +1,27 @@
 from sqlalchemy import DateTime
 from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.sql import expression
+from sqlalchemy.sql.expression import FunctionElement  # type: ignore[attr-defined]
 
 
 __all__ = ['utcnow']
 
 
-class utcnow(expression.FunctionElement):       # noqa Class names should use CamelCase convention
-    """ docs: https://docs.sqlalchemy.org/en/14/core/compiler.html#utc-timestamp-function """
+class UTCNow(FunctionElement):  # type: ignore[misc]
+    """
+    Docs:
+    https://docs.sqlalchemy.org/en/14/core/compiler.html#utc-timestamp-function
+    """
+
     type = DateTime()
 
 
-@compiles(utcnow, 'postgresql')
-def pg_utcnow(element, compiler, **kw):
+@compiles(UTCNow, 'postgresql')  # type: ignore[misc]
+def pg_utcnow(element, compiler, **kw) -> str:  # type: ignore[no-untyped-def]
     return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
+
+
+# Classes should be named by CamelCase.
+# Functions should be named by snake_case.
+# Since class represents `FunctionElement` (also means might be used as a function)
+# lowercase alias has been created.
+utcnow = UTCNow

21e5321b82679a0a803ea7c2e2a15acfbb36a1fc
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-22 21:39:01 +0200

    feat: improve db events

--- app/db/events.py
+++ app/db/events.py
@@ -2,11 +2,8 @@ import logging
 
 from fastapi import FastAPI
 
-from .postgres import (
-    create_engine,
-    create_sessionmaker
-)
-from ..core.config import sqlalchemy_connection_string
+from .state import DBState
+from ..core.settings.dataclasses_.components import DBSettings
 
 
 __all__ = [
@@ -18,16 +15,11 @@ __all__ = [
 logger = logging.getLogger(__name__)
 
 
-def init_db(app: FastAPI) -> None:
-    _set_db_in_app(sqlalchemy_connection_string, app)
-    logger.info('Database engine and sessionmaker have been set.')
-
-
-def _set_db_in_app(connection_string: str, app: FastAPI) -> None:
-    app.state.db_engine = create_engine(connection_string)
-    app.state.db_sessionmaker = create_sessionmaker(app.state.db_engine)
+def init_db(app: FastAPI, settings: DBSettings) -> None:
+    app.state.db = DBState(settings)
+    logger.info('Database state (engine and sessionmaker) has been set.')
 
 
 async def close_db(app: FastAPI) -> None:
-    await app.state.db_engine.dispose()
+    await app.state.db.engine.dispose()
     logger.info('Database engine has been disposed.')

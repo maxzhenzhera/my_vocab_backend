17f8e5934b4eff5f88740421514828f945be378e
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-23 12:09:56 +0200

    feat: improve helpers for api tests

--- tests/test_api/helpers/auth.py
+++ tests/test_api/helpers/auth.py
@@ -1,9 +1,9 @@
+from fastapi import FastAPI
 from httpx import AsyncClient
 
-from app.core.settings.dataclasses_.components import JWTSettings
 from app.schemas.authentication import AuthenticationResult
 from app.schemas.entities.user import UserInResponse
-from ..users import TestUser
+from ..dataclasses_ import MetaUser
 
 
 __all__ = [
@@ -14,58 +14,63 @@ __all__ = [
 ]
 
 
-CLIENT_USER_ATTRIBUTE_NAME = 'custom_attribute__user'
+_USER_ATTRIBUTE_NAME = 'custom_attribute__user'
 
 
 def set_user_in_client(
-        client: AsyncClient,
-        user: UserInResponse
+        user: UserInResponse,
+        client: AsyncClient
 ) -> None:
-    setattr(client, CLIENT_USER_ATTRIBUTE_NAME, user)
+    setattr(client, _USER_ATTRIBUTE_NAME, user)
 
 
 def get_user_from_client(client: AsyncClient) -> UserInResponse:
-    return getattr(client, CLIENT_USER_ATTRIBUTE_NAME)
+    return getattr(client, _USER_ATTRIBUTE_NAME)
 
 
 async def make_unauthenticated_client(
+        app: FastAPI,
         client: AsyncClient,
-        user: TestUser
+        meta_user: MetaUser
 ) -> AsyncClient:
-    user = await _send_create_user_request(client, user)
-    set_user_in_client(client, user)
+    user = await _send_create_user_request(app, client, meta_user)
+    set_user_in_client(user, client)
     return client
 
 
 async def _send_create_user_request(
+        app: FastAPI,
         client: AsyncClient,
-        user: TestUser
+        meta_user: MetaUser
 ) -> UserInResponse:
     response = await client.post(
-        # app.url_path_for('auth:create'),
-        client._protocol.app.url_path_for('auth:create'),
-        json=user.in_create.dict()
+        url=app.url_path_for('auth:create'),
+        json=meta_user.in_create.dict()
     )
     return UserInResponse(**response.json())
 
 
 async def authenticate_client(
+        app: FastAPI,
         client: AsyncClient,
-        user: TestUser
+        meta_user: MetaUser
 ) -> AsyncClient:
-    authentication_result = await _send_login_request(client, user)
-    _set_access_token_in_authorization_header(client, authentication_result.access_token)
+    authentication_result = await _send_login_request(app, client, meta_user)
+    _set_access_token_in_authorization_header(
+        client=client,
+        access_token=authentication_result.access_token
+    )
     return client
 
 
 async def _send_login_request(
+        app: FastAPI,
         client: AsyncClient,
-        user: TestUser
+        meta_user: MetaUser
 ) -> AuthenticationResult:
     response = await client.post(
-        # app.url_path_for('auth:login'),
-        client._protocol.app.url_path_for('auth:login'),
-        json=user.in_login.dict()
+        url=app.url_path_for('auth:login'),
+        json=meta_user.in_login.dict()
     )
     return AuthenticationResult(**response.json())
 
@@ -74,4 +79,4 @@ def _set_access_token_in_authorization_header(
         client: AsyncClient,
         access_token: str
 ) -> None:
-    client.headers['Authorization'] = f'{JWTSettings.access_token.type} {access_token}'
+    client.headers['Authorization'] = f'Bearer {access_token}'
--- tests/test_api/helpers/oauth.py
+++ tests/test_api/helpers/oauth.py
@@ -1,8 +1,9 @@
 from httpx import AsyncClient
 
+from app.db.models import User
 from app.db.repos import OAuthConnectionsRepo
 from .auth import get_user_from_client
-from ..users import TestUser
+from ..dataclasses_ import MetaUser
 
 
 __all__ = ['link_oauth_connections']
@@ -11,10 +12,12 @@ __all__ = ['link_oauth_connections']
 async def link_oauth_connections(
         oauth_connections_repo: OAuthConnectionsRepo,
         client: AsyncClient,
-        user: TestUser
+        meta_user: MetaUser
 ) -> None:
     created_user = get_user_from_client(client)
 
     async with oauth_connections_repo.session.begin():
-        for oauth_connection in user.get_oauth_connections(created_user.id):
-            await oauth_connections_repo.link_connection(oauth_connection)
+        await oauth_connections_repo.link_google_connection(
+            oauth_user=meta_user.google_oauth_user,
+            internal_user=User(id=created_user.id)
+        )

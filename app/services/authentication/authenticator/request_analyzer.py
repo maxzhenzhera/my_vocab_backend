65bfbae8e83fda6ae7566d7fa63d4b3182f20bd5
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-02-23 18:29:41 +0200

    feat: add type casts for for mypy

--- app/services/authentication/authenticator/request_analyzer.py
+++ app/services/authentication/authenticator/request_analyzer.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import cast
 
 from fastapi import Request
 
@@ -12,7 +13,7 @@ class RequestAnalyzer:
 
     @property
     def client_ip_address(self) -> str:
-        return self.request.client.host
+        return cast(str, self.request.client.host)
 
     @property
     def client_user_agent(self) -> str:

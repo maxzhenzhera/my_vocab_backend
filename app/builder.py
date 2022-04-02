720093af693db31be2115881704fd56b8d06b237
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-04-02 18:54:01 +0300

    feat: add logging

--- app/builder.py
+++ app/builder.py
@@ -1,12 +1,17 @@
+import logging
+import warnings
 from dataclasses import dataclass
 
 from fastapi import FastAPI
 
 from .core.settings import AppSettings
-from .core.settings.environments import AppEnvironmentType
+from .core.settings.environment import AppEnvType
 
 
 __all__ = ['AppBuilder']
+
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -14,19 +19,25 @@ class AppBuilder:
     settings: AppSettings
 
     def __post_init__(self) -> None:
-        self.app = FastAPI(**self.settings.fast_api.kwargs)
+        self.app = FastAPI(**self.settings.fastapi.kwargs)
 
-    def build_app(self) -> FastAPI:
+    @property
+    def short_app_info(self) -> str:
+        return f'< "{self.settings.fastapi.title}" [{self.settings.fastapi.version}] >'
+
+    def build(self) -> FastAPI:
         self._add_middlewares()
         self._add_event_handlers()
         self._add_exception_handlers()
         self._include_routers()
         self._inject_dependencies()
+        logger.info(f'App {self.short_app_info} has been built.')
         return self.app
 
     def _add_middlewares(self) -> None:
         self._add_session_middleware()
         self._add_cors_middleware()
+        logger.debug('Middlewares have been added.')
 
     def _add_session_middleware(self) -> None:
         from starlette.middleware.sessions import SessionMiddleware
@@ -35,6 +46,7 @@ class AppBuilder:
             SessionMiddleware,
             secret_key=self.settings.session.secret
         )
+        logger.debug('Session middleware has been added.')
 
     def _add_cors_middleware(self) -> None:
         from starlette.middleware.cors import CORSMiddleware
@@ -42,14 +54,16 @@ class AppBuilder:
         self.app.add_middleware(
             CORSMiddleware,
             allow_origins=self.settings.cors.origins,
-            allow_credentials=True,
-            allow_methods=self.settings.cors.origins,
+            allow_methods=self.settings.cors.methods,
             allow_headers=self.settings.cors.headers,
+            allow_credentials=True
         )
+        logger.debug('CORS middleware has been added.')
 
     def _add_event_handlers(self) -> None:
         self._add_startup_handler()
         self._add_shutdown_handler()
+        logger.debug('Event handlers have been added.')
 
     def _add_startup_handler(self) -> None:
         from .core.events import get_startup_handler
@@ -58,6 +72,7 @@ class AppBuilder:
             event_type='startup',
             func=get_startup_handler(self.app, self.settings)
         )
+        logger.debug('Startup handler has been added.')
 
     def _add_shutdown_handler(self) -> None:
         from .core.events import get_shutdown_handler
@@ -66,18 +81,27 @@ class AppBuilder:
             event_type='shutdown',
             func=get_shutdown_handler(self.app)
         )
+        logger.debug('Shutdown handler has been added.')
 
     def _add_exception_handlers(self) -> None:
         self._add_internal_server_exception_handler()
+        logger.debug('Exception handlers have been added.')
 
     def _add_internal_server_exception_handler(self) -> None:
-        if self.settings.env_type is AppEnvironmentType.DEV:
+        if self.settings.env_type is AppEnvType.DEV:
             from .api.errors.server import internal_server_exception_handler
 
             self.app.add_exception_handler(Exception, internal_server_exception_handler)
+            warnings.warn(
+                UserWarning(
+                    'Exception handler that shows traceback on internal server error '
+                    'has been added [Included for DEV environment].'
+                )
+            )
 
     def _include_routers(self) -> None:
         self._include_api_router()
+        logger.debug('Routers have been included.')
 
     def _include_api_router(self) -> None:
         from .api.routes import router
@@ -86,6 +110,7 @@ class AppBuilder:
             router=router,
             prefix='/api'
         )
+        logger.debug('API router has been included.')
 
     def _inject_dependencies(self) -> None:
         from .api.dependencies.authentication import inject_authentication
@@ -99,3 +124,4 @@ class AppBuilder:
         inject_mail(self)
         inject_oauth(self)
         inject_settings(self)
+        logger.debug('Dependencies have been injected.')

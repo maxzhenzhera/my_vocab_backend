4b9b6fe865d7d36c69e20b64545df90379f7d897
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-04-02 19:07:36 +0300

    feat: implement on entrypoing for different environments

--- app/__main__.py
+++ app/__main__.py
@@ -1,52 +1,17 @@
-import multiprocessing
-
-import gunicorn.app.base
+from typing import Final
 
 from app.core.config import get_app_settings
 from app.factory import get_app
+from app.utils.logging_.config import configure_base_logging
+from app.utils.runners.main import run
 
+
+configure_base_logging()
 
 settings = get_app_settings()
 app = get_app(settings)
 
-
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
-
-
-def handler_app(environ, start_response):
-    response_body = b'Works fine'
-    status = '200 OK'
-
-    response_headers = [
-        ('Content-Type', 'text/plain'),
-    ]
-
-    start_response(status, response_headers)
-
-    return [response_body]
-
-
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super().__init__()
-
-    def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
+APP_PATH: Final = 'app.__main__:app'
 
 if __name__ == '__main__':
-    options = {
-        'bind': '%s:%s' % ('127.0.0.1', '8080'),
-        'workers': number_of_workers(),
-    }
-    StandaloneApplication(handler_app, options).run()
+    run(APP_PATH, settings)

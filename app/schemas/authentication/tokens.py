2bf2a1a95367a24e45867b26641c5209ccee5897
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-04-02 18:45:42 +0300

    feat: add classmethods to build from settings

--- app/schemas/authentication/tokens.py
+++ app/schemas/authentication/tokens.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from ...core.settings.dataclasses_.components.tokens import TokenSettings
+
 
 __all__ = [
     'TokenInResponse',
@@ -11,6 +13,14 @@ class TokenInResponse(BaseModel):
     token: str
     token_type: str
     ttl: int
+
+    @classmethod
+    def from_settings(cls,  token: str, settings: TokenSettings) -> 'TokenInResponse':
+        return cls(
+            token=token,
+            token_type=settings.type,
+            ttl=settings.expire_in_seconds
+        )
 
 
 class TokensInResponse(BaseModel):

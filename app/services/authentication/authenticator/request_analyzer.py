a02bc8a9f07ac2235939958f72b887167b73ad91
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-04-02 18:49:18 +0300

    feat: add tmp feature of parsing client IP from X-Real-IP header due to starting working with reverse proxy (nginx)

--- app/services/authentication/authenticator/request_analyzer.py
+++ app/services/authentication/authenticator/request_analyzer.py
@@ -1,3 +1,4 @@
+import warnings
 from dataclasses import dataclass
 from typing import cast
 
@@ -7,13 +8,21 @@ from fastapi import Request
 __all__ = ['RequestAnalyzer']
 
 
+warnings.warn(
+    UserWarning(
+        'Client IP address handling have to be improved. '
+        'Reminder: `uvicorn`.`forwarded_allow_ips` - [*].'
+    )
+)
+
+
 @dataclass
 class RequestAnalyzer:
     request: Request
 
     @property
     def client_ip_address(self) -> str:
-        return cast(str, self.request.client.host)
+        return cast(str, self.request.client.host or self.request.headers['X-Real-IP'])
 
     @property
     def client_user_agent(self) -> str:

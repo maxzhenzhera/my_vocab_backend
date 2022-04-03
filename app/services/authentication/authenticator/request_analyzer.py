f2bbab71668326640b4b47d6ba0e1c9103a17099
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-04-03 15:36:15 +0300

    feat: retrieve client IP through request client

--- app/services/authentication/authenticator/request_analyzer.py
+++ app/services/authentication/authenticator/request_analyzer.py
@@ -1,4 +1,3 @@
-import warnings
 from dataclasses import dataclass
 from typing import cast
 
@@ -8,21 +7,13 @@ from fastapi import Request
 __all__ = ['RequestAnalyzer']
 
 
-warnings.warn(
-    UserWarning(
-        'Client IP address handling have to be improved. '
-        'Reminder: `uvicorn`.`forwarded_allow_ips` - [*].'
-    )
-)
-
-
 @dataclass
 class RequestAnalyzer:
     request: Request
 
     @property
     def client_ip_address(self) -> str:
-        return cast(str, self.request.client.host or self.request.headers['X-Real-IP'])
+        return cast(str, self.request.client.host)
 
     @property
     def client_user_agent(self) -> str:

c61578314028067f1b727154380e68451078e59a
Max Zhenzhera <maxzhenzhera@gmail.com>
2022-04-29 15:37:23 +0300

    fix & refactor: remove peppering and salting logic; sinplify methods' names

--- app/services/password/service.py
+++ app/services/password/service.py
@@ -3,10 +3,7 @@ import string
 from dataclasses import dataclass
 from typing import cast
 
-import bcrypt
-
 from .context import pwd_context
-from ...core.settings.dataclasses_.components import PasswordSettings
 from ...db.models import User
 
 
@@ -19,12 +16,7 @@ DEFAULT_RANDOM_PASSWORD_ALPHABET = string.ascii_letters + string.digits
 
 @dataclass
 class PasswordService:
-    settings: PasswordSettings
     user: User
-
-    @property
-    def pepper(self) -> str:
-        return self.settings.pepper
 
     @staticmethod
     def generate_random_password(
@@ -33,29 +25,16 @@ class PasswordService:
     ) -> str:
         return ''.join([secrets.choice(alphabet) for _ in range(length)])
 
-    def set_password(self, password: str) -> None:
-        self.user.salt = self._generate_salt()
-        self.user.hashed_password = self._hash_password(
-            self._pepper_salt_password(password)
-        )
+    def set(self, password: str) -> None:
+        self.user.hashed_password = self._hash(password)
 
-    def verify_password(self, password: str) -> bool:
-        return self._verify_password(
-            self._pepper_salt_password(password),
-            self.user.hashed_password
-        )
-
-    def _pepper_salt_password(self, password: str) -> str:
-        return self.pepper + self.user.salt + password
+    def verify(self, password: str) -> bool:
+        return self._verify(password, self.user.hashed_password)
 
     @staticmethod
-    def _generate_salt() -> str:
-        return bcrypt.gensalt().decode()
-
-    @staticmethod
-    def _hash_password(password: str) -> str:
+    def _hash(password: str) -> str:
         return cast(str, pwd_context.hash(password))
 
     @staticmethod
-    def _verify_password(plain_password: str, hashed_password: str) -> bool:
+    def _verify(plain_password: str, hashed_password: str) -> bool:
         return cast(bool, pwd_context.verify(plain_password, hashed_password))
